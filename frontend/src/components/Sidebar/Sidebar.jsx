97131dc4321e4e1ff1459b90a5f4e3bbdf5a1d10
greshh <greshkalao@gmail.com>
2026-01-19 12:23:14 +1300

    Temporary fix for logging out - user clicks on profile picture on bottom-left.

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useUser from "../../hooks/useUser";
+import { getAuth, signOut } from "firebase/auth";
 import "./style.css";
 
 export const Sidebar = (user) => {
   // const { isLoading, user } = useUser();
+
+  const auth = getAuth();
+  const navigate = useNavigate();
 
   return (
     <div className='sidebar'>
@@ -17,8 +21,16 @@ export const Sidebar = (user) => {
             src={`https://storage.googleapis.com/conference-planner/profile-pic/member/${user.member_id}.jpg`}
             className="user-avatar"
             alt={user.first_name}
-            title={`${user.first_name} ${user.last_name}`}
+            title={"Sign Out"}
             onError={(e) => { e.currentTarget.src = "https://storage.googleapis.com/conference-planner/profile-pic/unknown.jpg"; }}
+            onClick={()=>{
+              signOut(auth).then(() => {
+                navigate("/")
+              }).catch((err) => {
+                console.log("Unable to log out:", err.message);
+              });
+            }}
+            style={{ cursor: "pointer" }}
           />
           <p>{user.user.first_name} {user.user.last_name}</p>
         </div>
