d635deca7a34ccc950046442324a9b7e7c7f480f
greshh <greshkalao@gmail.com>
2026-01-19 19:02:40 +1300

    Edit URL for log-in image to work during deployment.

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import useUser from "../../hooks/useUser";
 import { getAuth, signOut } from "firebase/auth";
 import "./style.css";
 
